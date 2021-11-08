86a7b67ccdf1f473225d2f2e0c434f621d92f3f9
Martin Ingram <91732210+martin-ingram@users.noreply.github.com>
2021-11-08 13:51:20 +0000

    Update round.off.R
    updated digits from n

--- R/round.off.R
+++ R/round.off.R
@@ -1,9 +1,9 @@
 round.off <-
 function(x, digits = 0) {
   posneg = sign(x)
-  z = abs(x)*10^n
+  z = abs(x)*10^digits
   z = z + 0.5 + sqrt(.Machine$double.eps)
   z = trunc(z)
-  z = z/10^n
+  z = z/10^digits
   z*posneg
 }
